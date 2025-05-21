2ffc0b7832d027a09e89135ab7944028ece755db
mgaisnon <mathieu.gaisnon@gmail.com>
2025-05-21 17:50:58 +0200

    test fail

--- test/test.py
+++ test/test.py
@@ -4,5 +4,8 @@ class TestEnvironnement(unittest.TestCase):
     def test_true_est_true(self):
         self.assertTrue(True)
 
+    def testfailure(self):
+        self.assertTrue(False)
+
 if __name__ == "__main__":
     unittest.main()
