2264971756b89a42f36e9e80068992706dfaa4ed
JulieMontoux <julie.montoux@epsi.fr>
2025-05-22 09:54:58 +0200

    fix: test failure

--- test/test.py
+++ test/test.py
@@ -4,8 +4,5 @@ class TestEnvironnement(unittest.TestCase):
     def test_true_est_true(self):
         self.assertTrue(True)
 
-    def testfailure(self):
-        self.assertTrue(False)
-
 if __name__ == "__main__":
     unittest.main()
