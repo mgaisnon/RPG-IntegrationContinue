6ff9c7a93a520eedcc60da662c989165ed07361c
JulieMontoux <julie.montoux@epsi.fr>
2025-05-22 11:49:48 +0200

    fix : classe joueur pylint

--- RPG/joueur.py
+++ RPG/joueur.py
@@ -1,15 +1,20 @@
+"""Module joueur du RPG."""
 class Joueur:
+    """Classe représentant un joueur."""
     def __init__(self, nom, vie=10):
+        """Initialise un joueur avec son nom et ses points de vie."""
         self.nom = nom
         self.vie = vie
-        
+
     def attaque(self, dommage=1):
+        """Logique d'attaque du joueur."""
         self.vie -= dommage
         print(f"{self.nom} perd {dommage} point(s) de vie (PV restant: {self.vie}).")
         if self.vie <=0:
             print(f"{self.nom} est mort")
 
     def tour(self, cible):
+        """Logique de bataille entre les joueurs."""
         print(f"J1 : {self.nom} | PV : {self.vie}")
         print(f"J2 : {cible.nom} | PV : {cible.vie}")
         print(f"{self.nom} attaque {cible.nom}")
