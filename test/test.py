b5b23243fd2cffbe531f3e761721c394503521d7
youssef <yassin.farassi4@gmail.com>
2025-05-22 09:56:18 +0200

    Edit joueur.py + ajout test attaque

--- test/test.py
+++ test/test.py
@@ -1,11 +1,25 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import unittest
+from RPG.joueur import Joueur
 
 class TestEnvironnement(unittest.TestCase):
-    def test_true_est_true(self):
-        self.assertTrue(True)
+    def test_attaque_joueur(self):
+        j1 = Joueur("Maa")
+        j2 = Joueur("Matheuz")
 
-    def testfailure(self):
-        self.assertTrue(False)
+        j1.tour(j2)
+        assert j2.vie == 9
+        
+    # def test_fail_attaque_joueur2(self):
+    #     j1 = Joueur("Maa")
+    #     j2 = Joueur("Matheuz")
+
+    #     j1.tour(j2)
+    #     assert j2.vie == 10
+
 
 if __name__ == "__main__":
     unittest.main()
