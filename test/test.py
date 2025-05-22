ede22fe8e10e4d3765172dc28ba923089035ac12
mgaisnon <mathieu.gaisnon@gmail.com>
2025-05-22 12:15:07 +0200

    edit test attaque joueur

--- test/test.py
+++ test/test.py
@@ -5,15 +5,18 @@ import os
 import unittest
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from RPG.joueur import Joueur
+from unittest.mock import patch
 
 class TestEnvironnement(unittest.TestCase):
-    def test_attaque_joueur(self):
+    @patch('builtins.input', return_value='')  # Mock l'entrée utilisateur
+    def test_attaque_joueur(self, mock_input):
         """Teste si l'attaque retourne la bonne valeur (exemple)."""
         j1 = Joueur("Maa")
         j2 = Joueur("Matheuz")
 
         j1.tour(j2)
-        assert j2.vie == 9
+
+        self.assertEqual(j2.vie, 9)
         
     # def test_fail_attaque_joueur2(self):
     #     j1 = Joueur("Maa")
